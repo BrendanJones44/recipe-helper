0bf9083fb7c2e437a113d5af33179a8071904753
BrendanJones44 <bpj1651@rit.edu>
2019-05-08 22:22:37 -0700

    cleanup

--- src/List.js
+++ src/List.js
@@ -9,7 +9,7 @@ const List = props => {
   const listItems = Object.keys(items).map(function(item) {
     const count = items[item];
     return (
-      count > 0 ? <ListItem name={item} count={items[item]} onItemButtonPress={onItemButtonPress}/> : null
+      count > 0 ? <ListItem name={item} count={count} onItemButtonPress={onItemButtonPress}/> : null
     )
   });
   return (
